f5eba6036dec76df74c69c907daf42b0ff791d04
Kenneth <kenneth.lo@octopuspos.com>
2020-10-23 18:19:53 +0800

    Add Postman Collection link to ReadMe.md

--- app/index.js
+++ app/index.js
@@ -11,19 +11,55 @@ const { resolve } = require("path");
 
 app.use(bodyParser.json());
 
-const conn = mysql.createConnection({
-	host: DBConf.HOST,
-	user: DBConf.USER,
-	port: DBConf.PORT,
-	password: DBConf.PASSWORD,
-	database: DBConf.DATABASE,
-	multipleStatements: true,
-});
+// const conn = mysql.createConnection({
+// 	host: DBConf.HOST,
+// 	user: DBConf.USER,
+// 	port: DBConf.PORT,
+// 	password: DBConf.PASSWORD,
+// 	database: DBConf.DATABASE,
+// 	multipleStatements: true,
+// });
 
-conn.connect((error) => {
-	if (error) throw error;
-	console.log("mysql connected");
-});
+let conn = null;
+
+let handleDisconnect = () => {
+	conn = mysql.createConnection({
+		host: DBConf.HOST,
+		user: DBConf.USER,
+		port: DBConf.PORT,
+		password: DBConf.PASSWORD,
+		database: DBConf.DATABASE,
+		multipleStatements: true,
+	}); // Recreate the connection, since
+	// the old one cannot be reused.
+
+	conn.connect( (err) => {
+		// The server is either down
+		if (err) {
+			// or restarting (takes a while sometimes).
+			console.log("error when connecting to db:", err);
+			setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+		} // to avoid a hot loop, and to allow our node script to
+	}); // process asynchronous requests in the meantime.
+	// If you're also serving http, display a 503 error.
+	conn.on("error", (err)=>{
+		console.log("db error", err);
+		if (err.code === "PROTOCOL_CONNECTION_LOST") {
+			// Connection to the MySQL server is usually
+			handleDisconnect(); // lost due to either server restart, or a
+		} else {
+			// connnection idle timeout (the wait_timeout
+			throw err; // server variable configures this)
+		}
+	});
+}
+
+handleDisconnect();
+
+// conn.connect((error) => {
+// 	if (error) throw error;
+// 	console.log("mysql connected");
+// });
 
 var mentionRegex = /@([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi;
 const sqlGenerator = new SqlGenerator();
